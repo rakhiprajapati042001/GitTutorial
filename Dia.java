e3e490aaef9ba05994427401a3e496b72a1bc4f5
rakhiprajapati042001 <104709138+rakhiprajapati042001@users.noreply.github.com>
2022-07-07 16:49:31 +0530

    Add files via upload

---
+++ Dia.java
@@ -0,0 +1,27 @@
+import javax.swing.JOptionPane; 
+public class Dia {
+
+	public static void main(String[] args) {
+		String name =JOptionPane.showInputDialog("");
+                  System.out.println(name);
+
+             JOptionPane.showMessageDialog(null,"name");
+                 //   System.out.println(
+             System.out.println(name.substring(1,7));
+             String result=name+" "+  "Goood day";
+                JOptionPane.showMessageDialog(null,"result");
+
+
+           System.out.println(result);
+           JOptionPane.showMessageDialog(null,name);
+       System.out.println(name.toLowerCase());
+            JOptionPane.showMessageDialog(null,name);
+      System.out.println(name);
+       System.out.println(name.toUpperCase());
+       JOptionPane.showMessageDialog(null,name);
+      System.out.println(name);
+         
+
+	}
+
+}
